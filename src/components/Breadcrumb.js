5b2dffbd132aefa774d4a5cd89da3fbcef1d5cbc
hi <tommy_chester@hotmail.com>
2021-01-12 16:40:51 +0100

    Fixed an issue with the breadcrumb

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import Link from './Link';
 
-const Breadcrumb = ({data, className}) => {
-    // const links
-    
-    const comps = [];
+const Breadcrumb = ({data, className}) => {    
+    const rawCrumbs = [];
+    const lastItem = data.length-1;
     for ( let i=0; i<data.length; i++) {
-        if (i === 0) {
-            comps[comps.length] = <span>{data[i].name}</span>
-        } 
-        
-        if (i > 0) {
-            comps[comps.length] = <span> &gt; </span>; 
-            comps[comps.length] = <Link href={data[i].link} className={className} >{data[i].name}</Link>;
+        if (i < lastItem) {
+            rawCrumbs[rawCrumbs.length] = <Link href={data[i].link} className={className} >{data[i].name}</Link>;
+            rawCrumbs[rawCrumbs.length] = <span> &gt; </span>; 
         }
+
+        if (i === lastItem) {
+            rawCrumbs[rawCrumbs.length] = <span>{data[i].name}</span>
+        } 
     }
 
     let i = 0;
-    const crumbs = comps.map( (comp) => {
-        return <React.Fragment key={i++}>{comp}</React.Fragment>;
+    const crumbs = rawCrumbs.map( (rawCrumb) => {
+        return <React.Fragment key={i++}>{rawCrumb}</React.Fragment>;
     });
 
     return (
