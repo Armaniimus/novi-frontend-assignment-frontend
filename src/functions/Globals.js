fa748419b188c407ca54d5c9710b6f4625436984
hi <tommy_chester@hotmail.com>
2021-02-01 18:40:33 +0100

    added urlVars to globals class

--- src/functions/Globals.js
+++ src/functions/Globals.js
@@ -1,20 +1,35 @@
+import HandleCookie from './HandleCookie';
+
 class Globals {
+    __HandleCookie = HandleCookie;
     __reloadNr = 0; 
     __token = '';
     __accountLvl = 0;
     __reloadFunction = null;
+    __urlVars = {}
 
     setToken(token) {
         this.__token = token;
+        this.__HandleCookie.set('token', token);
     };
 
     getToken() {
         return this.__token;
     }
 
+    setUrlVars(urlVars) {
+        this.__urlVars = urlVars;
+    }
+
+    getUrlVars() {
+        return this.__urlVars;
+    }
+
     setAccountLvl(accountLvl) {
-        if (accountLvl >= 0 && accountLvl <= 2) {
+        accountLvl = Math.floor(accountLvl);
+        if (accountLvl >= 0 && accountLvl <= 2 && accountLvl !== '') {    
             this.__accountLvl = accountLvl;
+            this.__HandleCookie.set('accountLvl', accountLvl);
         } else {
             console.error('tried to set a invalid accountLvl');
         }
