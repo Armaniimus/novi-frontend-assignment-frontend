06cf77618ab5d889b88eb4e0ddc25ba4e3fa9d0a
hi <tommy_chester@hotmail.com>
2021-02-01 10:11:30 +0100

    liedbeheer pagina gemaakt

--- src/pages/LiedBeheerSpecific.js
+++ src/pages/LiedBeheerSpecific.js
@@ -3,6 +3,8 @@ import OwnApi from '../apis/OwnApi';
 import {HandleApiError} from '../functions/HandleError';
 
 import Breadcrumb from '../components/Breadcrumb';
+import Button from '../components/Button';
+import '../css/liedBeheerSpecific.css';
 
 const breadcrumbData = [
     {
@@ -15,9 +17,31 @@ const breadcrumbData = [
     }
 ];
 
+const onUpdate = (token, id, songtext) => {
+    console.log('update', id);
+
+    const request = async () => {
+        let requestData = new URLSearchParams();
+        requestData.append('token', token);
+        requestData.append('songid', id);
+        requestData.append('songtext', songtext);
+        const response = await OwnApi.post('/liedbeheer/update/songtext', requestData);
+
+        if (response.status === 200 && response.data.status === 'success') {
+            console.log(response.data);
+        } else {
+            HandleApiError(response);
+        }
+    }
+
+    request();
+}
+
 const LiedBeheerSpecific = ({urlVars, token}) => {
-    const [data, setData] = useState({});
-    const [renderedData, setRenderedData] = useState(null);
+    const [songId, setSongId] = useState('');
+    const [songNumber, setSongNumber] = useState('');
+    const [songTitle, setSongTitle] = useState('');
+    const [songText, setSongText] = useState('');
 
     useEffect( () => {
         const request = async () => {
@@ -25,8 +49,13 @@ const LiedBeheerSpecific = ({urlVars, token}) => {
             requestData.append('token', token);
             const response = await OwnApi.post(`/overview/${urlVars.id}`, requestData);
 
-            if (response.status === 200 && response.data.status === 'success') {
-                setData(response.data);
+            if (response.status === 200 && response.data.status === 'success' && response.data.songInfo !== undefined) {                
+                const info = response.data.songInfo;
+                setSongId(info.id);
+                setSongNumber(info.number);
+                setSongTitle(info.title);
+                setSongText(info.songText);
+
             } else {
                 HandleApiError(response);
             }
@@ -36,24 +65,7 @@ const LiedBeheerSpecific = ({urlVars, token}) => {
         } else {
             console.log('', urlVars);
         }
-    }, [setData, token, urlVars]);
-
-    useEffect( () => {
-        if (data.status === 'success') {
-            if (data.songInfo !== undefined) {
-                setRenderedData(
-                    <React.Fragment>
-                        <div>{data.songInfo.id}</div>
-                        <div>{data.songInfo.number}</div>
-                        <div>{data.songInfo.title}</div>
-                        <div>{data.songInfo.songText}</div>
-                    </React.Fragment>
-                );
-            } else {
-                console.error('songinfo undefined', data);
-            }
-        }
-    }, [data, setRenderedData]);
+    }, [token, urlVars]);
 
     return (
         <React.Fragment>
@@ -61,7 +73,27 @@ const LiedBeheerSpecific = ({urlVars, token}) => {
                 <div className='flex-block'>
                     <Breadcrumb data={breadcrumbData} className='breadCrumbItem'/>
 
-                    {renderedData}
+                    <div className='edit-container'>
+                        <div className='blue-bar'>
+                            <span>Number: {songNumber}</span>
+                        </div>
+                        <h2 className='edit-title'>{songTitle}</h2>
+                        <div className='song-input'>
+                            <textarea 
+                                type='text' 
+                                onChange={ 
+                                    e => {
+                                        setSongText(e.target.value) 
+                                        console.log(e.target.value);
+                                    }
+                                }
+                                value={songText}
+                            />
+                        </div>
+                    </div>    
+                    <Button className='info' callback={() => {onUpdate(token, songId, songText)}}>Update songtext</Button>               
+
+
                 </div>
             </div>
         </React.Fragment>
