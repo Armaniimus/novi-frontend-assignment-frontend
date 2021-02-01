3c139465880de0d8e48016fd3dc0248ffa39b536
hi <tommy_chester@hotmail.com>
2021-02-01 18:49:48 +0100

    changed logout page to destroy cookie and update the url

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
 import Globals from '../functions/Globals';
+import HandleCookie from '../functions/HandleCookie';
 
 const Logout = () => {
     useEffect( () => {
         Globals.setToken('');
         Globals.setAccountLvl(0);
+        HandleCookie.destroyCookie();
         Globals.reload();
     
+        const navEvent = new PopStateEvent('popstate');
+        window.history.pushState({}, '', '/');
+        window.dispatchEvent(navEvent);
+
         // eslint-disable-next-line
     }, []);
     return null;
