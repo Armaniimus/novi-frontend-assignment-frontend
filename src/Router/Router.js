5904c7ffb1d66f6037223ee0f0b8d5c7816f4bf0
hi <tommy_chester@hotmail.com>
2021-02-01 18:51:30 +0100

    updated router to use  the new urlVars from the Globals class and cleaned code a bit

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -17,6 +17,56 @@ import LiedBeheerSpecific from '../pages/LiedBeheerSpecific';
 import AccountBeheer from '../pages/AccountBeheer';
 import OverviewSpecific from '../pages/OverviewSpecific';
 
+const routes = [
+    {
+        path: '/',
+        page: <Home/>,
+        requiredLvl: 0
+    },
+    {
+        path: '/logout',
+        page: <Logout/>,
+        requiredLvl: 1
+    },
+    {
+        path: '/overview',
+        page: <Overview/>,
+        requiredLvl: 1
+    },
+    {
+        path: '/overview/{id}',
+        // page: <OverviewSpecific urlVars={urlVars}/>,
+        page: <OverviewSpecific/>,
+        requiredLvl: 1
+    },
+    {
+        path: '/accountbeheer',
+        page: <AccountBeheer/>,
+        requiredLvl: 2
+    },
+    {
+        path: '/liedbeheer',
+        page: <LiedBeheer />,
+        requiredLvl: 2
+    },
+    {
+        path: '/liedbeheer/{id}',
+        // page: <LiedBeheerSpecific urlVars={urlVars}/>,
+        page: <LiedBeheerSpecific/>,
+        requiredLvl: 2
+    },
+    {
+        path: '/404',
+        page: <FourOFour />,
+        requiredLvl: 0
+    },
+    {
+        path: '/login',
+        page: <Login />,
+        requiredLvl: 0
+    }
+];
+
 const checkMatchingPath = (path, currentPath) => {
     const returnObj = {testResult: null, newUrlVars: {} }
     if (path === currentPath) {
@@ -46,6 +96,7 @@ const checkMatchingPath = (path, currentPath) => {
                 }
             }
             returnObj.testResult = true;
+
             return returnObj;
         } 
     }
@@ -55,31 +106,25 @@ const checkMatchingPath = (path, currentPath) => {
 
 const Router = () => {
     const [debounceDispatch, setDebounceDispatch] = useState(0);
-    const [debounceRoutes, setDebounceRoutes] = useState(0);
     const [debounceRouteCheck, setDebounceRouteCheck] = useState(0);
 
-    const [routes, setRoutes] = useState([]);
     const [reload, setReload] = useState(0);
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
-    const [urlVars, setUrlVars] = useState({});
     const [routeParams, setRouteParams] = useState({page: null, requiredLvl: 0});
     const [renderedDispatchPage, setRenderedDispatchPage] = useState(null);
-    Globals.setReloadFunc( setReload );
-
-
-    useEffect( () => {
-        console.log('reloadUpdated', reload);
-    }, [reload])
+    
 
     //set a default value
     useEffect( () => {
-        setRouteParams({
-            page: <Login />, 
-            requiredLvl: 0
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        Globals.setReloadFunc( setReload );        
     }, []);
 
+    useEffect( () => {
+        routeCheck();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [reload]);
+
+    //setup page reload
     useEffect( () => {
         const onLocationChange = () => {
             setCurrentPath(window.location.pathname);
@@ -91,92 +136,44 @@ const Router = () => {
         return () => {
             window.removeEventListener('popstate', onLocationChange);
         }
-    }, [currentPath, reload, urlVars]);
+    }, [currentPath, reload]);
 
+    const routeCheck = () => {
+        let fourOfour = true;
+        routes.map( ({path, page, requiredLvl }) => {
+            const {testResult, newUrlVars} = checkMatchingPath(path, currentPath);
 
-    useEffect( () => {
-        clearTimeout(debounceRoutes);
-        setDebounceRoutes(
-            setTimeout(() => {
-                setRoutes([
-                    {
-                        path: '/',
-                        page: <Home/>,
-                        requiredLvl: 0
-                    },
-                    {
-                        path: '/logout',
-                        page: <Logout/>,
-                        requiredLvl: 1
-                    },
-                    {
-                        path: '/overview',
-                        page: <Overview/>,
-                        requiredLvl: 1
-                    },
-                    {
-                        path: '/overview/{id}',
-                        page: <OverviewSpecific urlVars={urlVars}/>,
-                        requiredLvl: 1
-                    },
-                    {
-                        path: '/accountbeheer',
-                        page: <AccountBeheer/>,
-                        requiredLvl: 2
-                    },
-                    {
-                        path: '/liedbeheer',
-                        page: <LiedBeheer />,
-                        requiredLvl: 2
-                    },
-                    {
-                        path: '/liedbeheer/{id}',
-                        page: <LiedBeheerSpecific urlVars={urlVars}/>,
-                        requiredLvl: 2
-                    },
-                    {
-                        path: '/404',
-                        page: <FourOFour />,
-                        requiredLvl: 0
-                    },
-                ]);
-            }, 50)
-        );
+            if (testResult) {
+                fourOfour = false;
+                                
+                if ( Compare.compare(Globals.getUrlVars(), newUrlVars) === false ) {
+                    Globals.setUrlVars(newUrlVars);
+                    setRouteParams( {page: page, requiredLvl: requiredLvl} );
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [urlVars, reload] );
+                } else {
+                    setRouteParams( {page: page, requiredLvl: requiredLvl} );
+                }
+            }
+            return null;
+        });
+
+        if (fourOfour) {
+            const navEvent = new PopStateEvent('popstate');
+            window.history.pushState({}, '', '/');
+            window.dispatchEvent(navEvent);
+            console.info('navigation','404');
+        }
+    }
 
     useEffect( () => {
         clearTimeout(debounceRouteCheck);
         setDebounceRouteCheck( 
             setTimeout ( () => {
-                let fourOfour = true;
-                routes.map( ({path, page, requiredLvl }) => {
-                    const {testResult, newUrlVars} = checkMatchingPath(path, currentPath);
-
-                    if (testResult) {
-                        fourOfour = false;
-                                        
-                        if ( Compare.compare(urlVars, newUrlVars) === false ) {
-                            setUrlVars(newUrlVars);
-
-                        } else {
-                            setRouteParams( {page: page, requiredLvl: requiredLvl} );
-                        }
-                    }
-                    return null;
-                });
-
-                if (fourOfour) {
-                    const navEvent = new PopStateEvent('popstate');
-                    window.history.pushState({}, '', '/');
-                    window.dispatchEvent(navEvent);
-                    console.info('navigation','404');
-                }
+                routeCheck();
             } , 50)
         );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentPath, routes, reload]);
+    }, [currentPath, reload]);
 
     useEffect( () => {
         clearTimeout(debounceDispatch);
@@ -190,7 +187,7 @@ const Router = () => {
 
     return (
         <React.Fragment>
-            <Header />
+            <Header/>
             {renderedDispatchPage}
             <Footer />
         </React.Fragment>
