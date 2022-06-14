d8f034cb7d49bd7ff7a291fca6745ef150bbcec3
Armaniimus <tommy_chester@hotmail.com>
2022-06-14 03:44:11 +0200

    fixed link bug breadcrumb

--- src/components/Breadcrumb/Breadcrumb.js
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import Link from '../Link/Link';
-import Globals from '../../functions/Globals';
-
 
 import RawStyles from './Breadcrumb.module.css';
-
-const pageConst = Globals.pageConst;
 
 const Breadcrumb = ({data, className}) => {    
     const rawCrumbs = [];
     const lastItem = data.length-1;
     for ( let i=0; i<data.length; i++) {
         if (i < lastItem) {
-            rawCrumbs[rawCrumbs.length] = <Link href={`${pageConst}/${data[i].link}`} className={className} >{data[i].name}</Link>;
+            rawCrumbs[rawCrumbs.length] = <Link href={`${data[i].link}`} className={className} >{data[i].name}</Link>;
             rawCrumbs[rawCrumbs.length] = <span> &gt; </span>; 
         }
 
